15ba3bce0983f3961f4e4d91c178b1dd5fab6140
Tiago Ferreira <ferreiratiago@outlook.com>
2024-08-20 21:22:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
 
-const password_token = 'super-secret-f8ed84e8f41e4146403dd4a6bbcea5e418d24i7';
+const hello_world = 'f8ed84e8f41e4146403dd4a6bbcea5e418d24i7';
 
 // Routes
 app.use(routes.current_user);
